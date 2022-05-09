84804da517f4625bc34a3452f225d3c76973334e
JodyGs <demajn@tuta.io>
2022-05-09 19:44:07 +0200

    Build sidebar, feed, widget and tweetbox

---
+++ components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/outline'
+import twitterLogo from '../public/assets/logo-twitter.png'
+import SidebarRow from './SidebarRow'
+
+function Sidebar() {
+  return (
+  <div className='col-span-2 flex flex-col items-center px-4 md:items-start '>
+    <img className='h-10 w-10 m-3' src='/assets/logo-twitter.png' alt="twitter logo" />
+    <SidebarRow Icon={HomeIcon} title="Home" />
+    <SidebarRow Icon={HashtagIcon} title="Explore" />
+    <SidebarRow Icon={BellIcon} title="Notifications" />
+    <SidebarRow Icon={MailIcon} title="Messages" />
+    <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+    <SidebarRow Icon={CollectionIcon} title="Lists" />
+    <SidebarRow Icon={UserIcon} title="Sign In" />
+    <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+  </div>
+  )
+}
+
+export default Sidebar
